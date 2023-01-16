569a4253a84213dae3be15c9d575c6514ad9dfe7
Timothy Chu <timchu@google.com>
2023-01-15 21:47:09 -0800

    Same task but in the Google extension popup (rather than a dedicated separate popup window)

--- content.js
+++ content.js
@@ -9,18 +9,14 @@ const url = document.URL;
 let key = 0;
 
 function onclick(myImageSrc){
-  console.log("clicked");
   var existingImgKeys;
   chrome.storage.local.get([url]).then((keyImgs) => {
     existingImgKeys = keyImgs[url]
     if (!existingImgKeys){
       existingImgKeys = {}
     }
-    console.log("Existing image keys: " + JSON.stringify(existingImgKeys));
     existingImgKeys[key.toString()] = myImageSrc;
     key += 1;
     chrome.storage.local.set({[url]: existingImgKeys})
-    console.log(url);
-    console.log("Existing image keys at end: " + JSON.stringify(existingImgKeys));
   });
 }
--- popup.js
+++ popup.js
@@ -1,30 +1,27 @@
 // Access current tab URL, required for accessing right data from storage.
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-  let url = tabs[0].url;
+  var url = tabs[0].url;
   // use `url` here inside the callback because it's asynchronous!
-  console.log("Tabs query workd")
   keyImgs = chrome.storage.local.get([url]).then((result) => {
     keyImgs = result[url]
-    console.log("Keys and Images: " + keyImgs)
     for (key in keyImgs){
       document.body.innerHTML = '<div class=image-container><br><div class="close-button">X</div><img style="max-height:400px; max-width:400px; height:auto; width: auto" src = "' + keyImgs[key] + '" key="' + key + '"></div>' + document.body.innerHTML;
     } 
     let close_buttons = document.getElementsByClassName("close-button")
     for (i = 0; i < close_buttons.length; i++){
-      console.log("Close button obtained!");
-      close_buttons[i].addEventListener("click", removeImage(url));
+      close_buttons[i].addEventListener("click", removeImage);
+      close_buttons[i].myUrl = url;
     }
   });
 });
 
-// Sort of works
-function removeImage(url) {
-  console.log(this.parentNode.outerHTML);
-  key = this.parentNode.children[1].getAttribute('key');
-  let keyImgs = chrome.storage.local.get([url])
-  delete keyImgs[key]
-  console.log("Removing parentNode");
-  console.log(this.outerHTML);
+function removeImage(evt) {
+  let url = evt.currentTarget.myUrl;
+  key = this.parentNode.children[2].getAttribute('key');
   this.parentNode.remove();
+  chrome.storage.local.get([url]).then((keyImgs) =>{
+    delete keyImgs[url][key]
+    chrome.storage.local.set({[url]: keyImgs[url]})
+  });
   //element.parentNode.remove();
 };
